9c6ca8de969ca8318eeb6083e3f28f53537d2f77
tahrighayth-sys <tahrighayth@gmail.com>
2026-03-15 08:03:28 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { Home,Picture,Card,Cake,Present } from "./components"
-import './index.css'
+import React from 'react';
+import Home from './components/Home';
+import Card from './components/Card';
 
 function App() {
-
   return (
-    <Router>
-      <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/pictures" element={<Picture/>}/>
-          <Route path="/card" element={<Card/>}/>
-          <Route path="/cake" element={<Cake/>}/>
-          <Route path="/present" element={<Present/>}/>
-      </Routes>
-
-    </Router>
-  )
+    <div className="App">
+      <Home />
+      <div style={{padding: '20px'}}>
+        <Card />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
