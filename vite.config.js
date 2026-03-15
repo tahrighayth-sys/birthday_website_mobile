ea81d665189679e896f59d6c6747868ac468974a
tahrighayth-sys <tahrighayth@gmail.com>
2026-03-15 08:23:45 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/birthday_website_mobile/",
+  build: {
+    outDir: './' // يخرّج الخدمة في الصفحة الرئيسية
+  }
 })
